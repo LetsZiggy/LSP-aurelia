ed77d8281380dfa21bcc33d9c59b63cd5d031244
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2023-03-07 17:51:51 +0800

    feat: server from github (root folder)

--- plugin.py
+++ plugin.py
@@ -1,3 +1,5 @@
+import os
+
 from lsp_utils import NpmClientHandler
 
 
@@ -13,3 +15,7 @@ class LspAureliaPlugin(NpmClientHandler):
 	package_name = __package__
 	# server_directory = "server" # TODO: edit once working
 	# server_binary_path = os.path.join(server_directory) # TODO: edit once working
+
+	# test - server from https://github.com/LetsZiggy/aurelia-language-server
+	server_directory = "server-github"
+	server_binary_path = os.path.join(server_directory, "node_modules", "aurelia", "out", "server.js")
